dd7bd11650a65a379a1fd0bb51e0e091f5cf3960
mac-mini <lexx.mg@gmail.com>
2022-04-02 10:47:25 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -2,21 +2,26 @@
 'use strict';
 
 const IP = '192.168.0.101'//'185.35.160.110:8101';
-const OUT = 4;
+const OUT_ARR = [1, 2, 3 , 5, 9];
 
 if (window.location.pathname === '/') {
-  const btn = document.querySelector('.content-control__btn');
+  const container = document.querySelector('.content-control');
+
+  addBtnToContainer(container, OUT_ARR);
+  isActive(IP);
 
   setInterval(() => {
-    isActiveOut(IP, OUT, btn);
+    isActive(IP);
   }, 1000);
 
-  btn.addEventListener('click', () => {
-    fetch(`/php/laurent.php?ip=${IP}&out=${OUT}&st=toggle`)
+  container.addEventListener('click' , event => {
+    const out = event.target.dataset.out;
+
+    fetch(`/php/laurent.php?ip=${IP}&out=${out}&st=toggle`)
       .then(res => res.text())
       .then(data => {
         console.log(data);
-        isActiveOut(IP, OUT, btn);
+        isActive(IP);
       })
   });
 }
@@ -29,14 +34,43 @@ if (window.location.pathname === '/route/auth/') {
   });
 }
 
-function isActiveOut(ip, out, element) {
+
+function addBtnToContainer(container, outArr) {
+  if (outArr.length <= 1) {
+    outArr.forEach((item, i) => {
+      container.insertAdjacentHTML('beforeend' , `
+        <button class="content-control__btn btn-control" data-out="${item}">on/off out-${item}</button>
+      `);
+    });
+  } else {
+    container.classList.add('content-control--many');
+
+    outArr.forEach((item, i) => {
+      container.insertAdjacentHTML('beforeend' , `
+        <button class="content-control__btn btn-control content-control__btn--many" data-out="${item}">on/off out-${item}</button>
+      `);
+    });
+  }
+}
+
+function isActive(ip) {
+  const allBtn = document.querySelectorAll('.content-control__btn');
+
   fetch(`/php/laurent.php?ip=${ip}`)
     .then(res => res.json())
     .then(data => {
-      if ( +data.out_table0.split('')[out - 1] ) {
-        element.classList.add('content-control__btn--active');
-      } else {
-        element.classList.remove('content-control__btn--active');
-      }
+      const outActive = data.out_table0.split('');
+
+      outActive.forEach((item, i) => {
+        allBtn.forEach((btn, j) => {
+          if (i === btn.dataset.out - 1) {
+            if (item == 1) {
+              btn.classList.add('content-control__btn--active');
+            } else {
+              btn.classList.remove('content-control__btn--active');
+            }
+          }
+        });
+      });
     });
 }
--- templates/control.php
+++ templates/control.php
@@ -1,3 +1,3 @@
 <div class="content-control">
-  <button class="content-control__btn">on/off</button>
+  
 </div>
