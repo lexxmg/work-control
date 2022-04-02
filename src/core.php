c83202afc60f33fe4fe65ea4d5db8aba5ed59729
mac-mini <lexx.mg@gmail.com>
2022-04-02 07:18:08 +0300

    edit auth core.php

--- src/core.php
+++ src/core.php
@@ -1,11 +1,13 @@
 <?php
 
-$key = htmlspecialchars($_POST['key'] ?? '');
 $pathReguest = $_SERVER['DOCUMENT_ROOT'] . '/authorization/reguest.json';
 $pathAccessKey = $_SERVER['DOCUMENT_ROOT'] . '/authorization/accesss-key.json';
 
 $reguestArr = [];
 $arrAccessKey = [];
+
+$key = explode(':', htmlspecialchars($_POST['key'] ?? ''));
+
 
 if ( json_decode(file_get_contents($pathReguest), true) ) {
     $reguestArr = json_decode(file_get_contents($pathReguest), true);
@@ -15,10 +17,10 @@ if ( json_decode(file_get_contents($pathAccessKey), true) ) {
     $arrAccessKey = json_decode(file_get_contents($pathAccessKey), true);
 }
 
-if ( isset($_POST['submitKey']) ) {
-    setcookie('key', $key, time() + (3600 * 24 * 365), '/');
+if ( isset($_POST['submitKey']) && count($key) == 2 && $key[0] == 'key' && $key[1] != '' ) {
+    setcookie('key', $key[1], time() + (3600 * 24 * 365), '/');
 
-    $reguestArr[] = ['date'=> date('d.m.Y H:i:s'), 'key'=> $key];
+    $reguestArr[] = ['date'=> date('d.m.Y H:i:s'), 'key'=> $key[1]];
 
     file_put_contents( $pathReguest, json_encode($reguestArr) );
 }
