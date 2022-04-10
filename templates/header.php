933d9ab96b4f6d0ff05bc10f21e7df5fb5d093cf
mac-mini <lexx.mg@gmail.com>
2022-04-10 10:48:13 +0300

    sync

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@
     <title>work-control</title>
 
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
     <link rel="manifest" href="/img/favicon/site.webmanifest">
